071a441dabc06e2abfbaef3c0ad5aec28eb828d9
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2022-12-03 00:18:40 +0100

    Completed lesson how to build a Custom Tab Bar

--- CwCCustomTabBar/Views/Custom Tab Bar/CustomTabBar.swift
+++ CwCCustomTabBar/Views/Custom Tab Bar/CustomTabBar.swift
@@ -7,14 +7,61 @@
 
 import SwiftUI
 
+enum Tabs: Int {
+    case chats = 0
+    case contacts = 1
+}
+
 struct CustomTabBar: View {
+    
+    @Binding var selectedTab: Tabs
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        HStack(alignment: .center) {
+            
+            Button {
+                // Switch to chats
+                selectedTab = .chats
+            } label: {
+                
+                TabBarButton(buttonText: "Chats",
+                             imageName: "bubble.left",
+                             isActive: selectedTab == .chats)
+            }
+            .tint(Color("icons-secondary"))
+            
+            Button {
+                // New chat
+            } label: {
+                
+                VStack(alignment: .center, spacing: 4) {
+                    Image(systemName: "plus.circle.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 32, height: 32)
+                    Text("New Chat")
+                }
+            }
+            .tint(.blue)
+            
+            Button {
+                // Switch to contacts
+                selectedTab = .contacts
+            } label: {
+                
+                TabBarButton(buttonText: "Contacts",
+                             imageName: "person",
+                             isActive: selectedTab == .contacts)
+            }
+            .tint(Color("icons-secondary"))
+        }
+        .frame(height: 82)
     }
 }
 
 struct CustomTabBar_Previews: PreviewProvider {
     static var previews: some View {
-        CustomTabBar()
+        CustomTabBar(selectedTab: .constant(.contacts))
     }
 }
